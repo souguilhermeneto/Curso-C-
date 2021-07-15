10e967a13488dba7ce1c92d4f5a1667764abaeb5
souguilhermeneto <84298363+souguilhermeneto@users.noreply.github.com>
2021-07-15 14:05:01 -0300

    Add files via upload

---
+++ Inicial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace inicial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Meu primeiro programa C#!");
+            Console.WriteLine("Digite o que você tem no seu bolso");
+
+            string bolso =Console.ReadLine();
+            Console.WriteLine("No bolso tem: " + bolso);   
+    
+        }
+    }
+}
